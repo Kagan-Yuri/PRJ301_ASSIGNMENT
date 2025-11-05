3156d1746d0bc227bcee6cbf37ce7be027591d9b
Kagan-Yuri <dh590350@gmail.com>
2025-11-05 12:01:06 +0700

    DBContext
    Delete comment about author

--- PRJ301_ASM/src/java/dal/DBContext.java
+++ PRJ301_ASM/src/java/dal/DBContext.java
@@ -9,10 +9,6 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author sonnt
- */
 public abstract class DBContext<T> {
     protected Connection connection = null;
     
